056dadecb5b269b5491022c1a277c7bd808dbf23
Woojin Kim <wk22076@my.westlake.school.nz>
2024-10-18 10:00:04 +1300

    added walls of code to html files

---
+++ courses.py
@@ -0,0 +1,90 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
+
+app = Flask(__name__)
+
+app.secret_key =  "supersecretkey"
+
+def get_db_connection():
+    conn = sqlite3.connect('games.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/games')
+def view_games():
+    conn = get_db_connection()
+    games = conn.execute('SELECT * FROM games').fetchall()
+    conn.close()
+    return render_template('view_games.html', games = games)
+
+@app.route('/add', methods = ('GET', 'POST'))
+def add_game():
+    if request.method == 'POST':
+        title = request.form['title']
+        platform = request.form['platform']
+        genre = request.form['genre']
+        year = request.form['year']
+        sales = request.form['sales']
+
+        if not title or not platform or not year or not sales:
+            flash('All fields are required')
+        else:
+            conn = get_db_connection()
+            conn.execute('INSERT INTO games (title, platform, genre, year, sales) VALUES (?, ?, ?, ?, ?)', (title, platform, genre, year, sales)) 
+
+        conn.commit()
+        conn.close()
+        return redirect (url_for('view_games'))
+
+    return render_template('add_game.html')
+
+@app.route('/edit/<int:id>', methods = ('GET', 'POST'))
+def edit_game(id):
+    conn = get_db_connection()
+    game = conn.execute('SELECT * FROM games WHERE id = ?', (id,)).fetchone()
+
+    if request.method == 'POST':
+        title = request.form['title']
+        platform = request.form['platform']
+        genre = request.form['genre']
+        year = request.form['year']
+        sales = request.form['sales']
+
+        if not title or not platform or not year or not sales:
+            flash('All fields are required')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE games SET title = ?, platform = ?, genre = ?, year = ?, sales = ? WHERE id = ?', (title, platform, genre, year, sales, id)) 
+
+            conn.commit()
+            conn.close()
+            return redirect (url_for('view_games')) 
+
+    return render_template('edit_game.html', game = game)
+
+@app.route('/delete/<int:id>', methods=('GET', 'POST'))
+def delete_game(id):
+    conn = get_db_connection()
+    game = conn.execute('SELECT * FROM games WHERE id = ?', (id,)).fetchone()
+
+    if request.method == 'POST':
+        conn.execute('DELETE FROM games WHERE id = ?', (id,))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('view_games'))
+
+    return render_template('delete_game.html', game=game)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
